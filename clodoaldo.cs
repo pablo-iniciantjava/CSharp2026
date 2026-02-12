1f25bb9b87567b4ea4e820830849d2e257204a5d
pablo-iniciantjava <pabloviniciiussp@hotmail.com>
2026-02-11 21:20:31 -0300

    primeiro codigo curso c# SENAI AMERICANA

---
+++ clodoaldo.cs
@@ -0,0 +1,14 @@
+using System; //Biblioteca do C# (system ) para usar comandos de console
+
+public class Program//Declaração da classe
+{
+    public static void Main() //Método principal
+    {
+        //Imprime a mensagem no console
+        Console.WriteLine("Senai! Americana SP");
+        Console.WriteLine("aluno: Clodoaldo");
+
+
+
+    }
+}

8bcbde73b35b31d9e2e0ca11ed6969709d057903
Pablo <pabloviniciussp@hotmail.com>
2026-03-11 21:45:36 -0300

    Atualizao tuplas

---
+++ Aula.12/tuplas.cs
@@ -0,0 +1,9 @@
+// Tuplas em C#
+
+var clodoaldo = ("Clodoaldo", 34, 5000.34, "Heitor", "SENAI", 'B'); // Criando uma tupla com nome, idade e profissão
+
+System.Console.WriteLine($"Idade do clodoaldo: {clodoaldo.Item1}"); // Imprime a tupla completa
+System.Console.WriteLine($"Idade do clodoaldo: {clodoaldo.Item2}");
+System.Console.WriteLine($"Nome do filho do clodoaldo: {clodoaldo.Item3}"); 
+clodoaldo.Item1 = 54; // Acessando o nome do filho do clodoaldo
+System.Console.WriteLine($"Idade do clodoaldo: {clodoaldo.Item1}"); // Imprime a idade do clodoaldo

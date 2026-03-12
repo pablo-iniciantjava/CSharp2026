3b8117a2e10d15257eb5578ac119024b49ffb460
Pablo <pabloviniciussp@hotmail.com>
2026-03-11 21:51:03 -0300

    tuplas

--- Aula.12/tuplas.cs
+++ Aula.12/tuplas.cs
@@ -7,3 +7,7 @@ System.Console.WriteLine($"Idade do clodoaldo: {clodoaldo.Item2}");
 System.Console.WriteLine($"Nome do filho do clodoaldo: {clodoaldo.Item3}"); 
 clodoaldo.Item1 = 54; // Acessando o nome do filho do clodoaldo
 System.Console.WriteLine($"Idade do clodoaldo: {clodoaldo.Item1}"); // Imprime a idade do clodoaldo
+
+(int, int, int) numeros = (1, 2, 5); // Criando uma tupla de números inteiros
+
+System.Console.WriteLine($"Números: {numeros.Item1 + numeros.Item2 + numeros.Item3}"); // Imprime a soma dos números da tupla 
